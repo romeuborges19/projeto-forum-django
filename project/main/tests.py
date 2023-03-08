4d9936c33713da9e03f04bd5b6cd469dff0ce55a
Romeu Borges <romeuborges19@gmail.com>
2023-03-08 17:48:36 -0300

    Organizing partials and adjusting user posts

--- project/main/tests.py
+++ project/main/tests.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
 
-class Test_Oi(TestCase):
-    def test_basic(self):
-        x = 'this'
-        assert 'h' in x
+class TestMainURLs(TestCase):
+    def test_home_url_is_correct(self):
+        home_url = reverse('main:home')
+        self.assertEqual(home_url, '/')
+
+    def test_ask_url_is_correct(self):
+        ask_url = reverse('main:ask')
+        self.assertEqual(ask_url, '/ask/')
 
 
--- project/main/views.py
+++ project/main/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import QuestionForm
+from .models import Question
 # Create your views here.
 
+app_name = 'main'
+
 def home(request):
-    return render(request, 'main/pages/main.html')
+    questions = Question.objects.all()
+
+    return render(request, 'main/pages/main.html', context={
+        'questions':questions
+    })
 
 def ask(request):
     form = QuestionForm()
+
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            form.save()
+
     return render(request, 'main/pages/asking_page.html', context={'form': form})
